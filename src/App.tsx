b83f5f16d1663f71da8510c3b7e0772c3af7e7ea
Suparna Debnath <debnathsuparna7503@gmail.com>
2026-09-17 23:54:51 +0600

    json data done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,34 @@
 import Navber from './component/shared/Navber'
 import Banner from './component/shared/Banner'
 
+const fetchtechs = async () => {
+	const res = await fetch('/public/technologies.json')
+	const data = await res.json()
+	return data
+}
+
+const techPromise = fetchtechs()
 const App = () => {
   return (
     <div>
    <Navber />
    <Banner />
+   <main>
+				<section className='container mx-auto my-10'>
+					<div className='grid grid-cols-4 gap-5'>
+					
+						<Suspense fallback={<div>loading...</div>}>
+							<Books
+								handleSavedtech={handleSavedtech}
+								techPromise={techPromise}
+								saved={saved}
+							/>
+						</Suspense>
+
+			
+					</div>
+				</section>
+			</main>
     </div>
   );
 };
